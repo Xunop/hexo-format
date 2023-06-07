49ce1fb4a3edd01959fc68fbe907dac00ffe956f
xun <xun794@gmail.com>
2023-06-07 21:21:15 +0800

    modified script

--- format.sh
+++ format.sh
@@ -18,8 +18,27 @@ ALL=0
 FILE=""
 
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+# Define functions for printing colored output
+print_error () {
+  printf "${RED}[ERROR] ${1}${NC}\n"
+}
+
+print_info () {
+  printf "${GREEN}[INFO] ${1}${NC}\n"
+}
+
+print_warning () {
+  printf "${YELLOW}[WARNING] ${1}${NC}\n"
+}
+
 function handle_error {
-  echo -e "\033[31mError:\033[0m" "$1" at $2 >&2
+  print_error "$1" at $2 >&2
   exit 1
 }
 
@@ -66,7 +85,7 @@ function parse_args {
         exit 0
         ;;
       *)
-        echo "Error: Unrecognized option $key. Use --help to see the usage." >&2
+        print_error "Error: Unrecognized option $key. Use --help to see the usage." >&2
         exit 1
         ;;
     esac
@@ -109,8 +128,13 @@ function is_desc {
 function gen_head_info {
     # get article title
     TITLE=$(sed -n '1,20{/^[#][^#]/p}' "$1" | awk '{print $2}')
-    # generate head info
-    head_info="---\ndate: ${DATE}\ntitle: ${TITLE}\ndescription: ${DESC}\n---"
+    # DESC is not empty
+    if [ -n "$DESC" ]; then
+        # generate head info
+        head_info="---\ndate: ${DATE}\ntitle: ${TITLE}\ndescription: ${DESC}\n---"
+    else
+        head_info="---\ndate: ${DATE}\ntitle: ${TITLE}\n---"
+    fi
     # replace '\' and '"'
     head_info=$(echo $head_info | sed 's/\\/\\\\/g' | sed 's/\"/\\\"/g')
 }
@@ -131,35 +155,52 @@ function peocess_file {
     filename=$(basename "$1")
     TITLE=$(sed -n '1,20{/^[#][^#]/p}' "$1" | awk '{print $2}')
     if [ ! -d $SOURCE_DIR/$dir ]; then
-        echo "--> create folder $SOURCE_DIR/$dir"
+        print_info "--> create folder $SOURCE_DIR/$dir"
         mkdir -p $SOURCE_DIR/$dir || handle_error "Could not create folder $SOURCE_DIR/$dir" $LINENO
     fi
 
     # first line is '---'
     if head -n 1 "$1" | grep -q "^---$"; then
         # get line number of second '---'
+        # if there is not a '---', yhln=1
         yhln=$(sed -n '2,20{/^---/{=;q}}' "$1")
         ln=$((yhln+6))
     fi
     
+    # no head info
+    if [ $yhln == 1 ]; then
+        print_info "this article has no head info, generate head info"
+        gen_head_info "$1"
+    fi
     # determine if there is need a description
     if is_desc "$1"; then
+        print_info "this article need a description"
         get_desc "$1"
-        if [ -n "$DESC" ]; then
-            # have head info
-            if [ $yhln != 1 ]; then
+        # head_info is empty
+        # this article has head info
+        if [ -z "$head_info" ]; then
+            # DESC is not empty
+            if [ -n "$DESC" ]; then
                 sed "1a description: $DESC" "$1" > $SOURCE_DIR/$dir/$filename \
                     || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
-            else
-                gen_head_info "$1"
-                sed "1i $(echo -e $head_info)" "$1" \
-                    > $SOURCE_DIR/$dir/$filename || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+                                    return
             fi
-            return
+        else
+            sed "1i $(echo -e $head_info)" "$1" \
+                > $SOURCE_DIR/$dir/$filename || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+            head_info=""
+            # DESC is not empty then return
+            if [ -n "$DESC" ]; then return; fi
         fi
     fi
-
-    echo "oh! $SOURCE_DIR/$dir/$filename is need a <!-- more -->"
+    
+    echo -e "$head_info"
+    # insert head info
+    if [ -n "$head_info" ]; then
+        sed "1i $(echo -e $head_info)" "$1" > "temp" || handle_error "Error $SOURCE_DIR/$dir/$filename" $LINENO
+    fi
+    
+    print_info "oh! $SOURCE_DIR/$dir/$filename is need a <!-- more -->"
 
     # check if the line is a block
     hline=$(sed -n '1,20{/^```/{=;q}}' $1)
@@ -183,28 +224,39 @@ function peocess_file {
         # insert '<!-- more -->'
         # remove titles that start with '#' but not '##...'
         # write to file
-        sed '1,20{/<!-- more -->/d}' "$1" | sed "${ln}i \\\n<!-- more -->" | sed '1,20{/^[#][^#]/d}' > $SOURCE_DIR/$dir/$filename \
-            || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
-        echo "--> saved $SOURCE_DIR/$dir/$filename"
-    else
-        echo "--> insert <!-- more --> to $SOURCE_DIR/$dir/$filename"
         
-        # insert '<!-- more -->'
-        sed "${ln}i \\\n<!-- more -->" "$1" | sed '1,20{/^[#][^#]/d}' > $SOURCE_DIR/$dir/$filename \
-            || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+        if [ -n "$head_info" ]; then
+            sed '1,20{/<!-- more -->/d}' "$1" | sed "${ln}i \\\n<!-- more -->" | sed '1,20{/^[#][^#]/d}' | sed "1i $(echo -e $head_info)" > $SOURCE_DIR/$dir/$filename \
+                || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+        else
+            sed '1,20{/<!-- more -->/d}' "$1" | sed "${ln}i \\\n<!-- more -->" | sed '1,20{/^[#][^#]/d}' > $SOURCE_DIR/$dir/$filename \
+                || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+        fi
+        print_info "--> saved $SOURCE_DIR/$dir/$filename"
+    else
+        print_info "--> insert <!-- more --> to $SOURCE_DIR/$dir/$filename"
+        
+        if [ -n "$head_info" ]; then
+            # insert '<!-- more -->'
+            sed "${ln}i \\\n<!-- more -->" "$1" | sed '1,20{/^[#][^#]/d}' | sed "1i $(echo -e $head_info)" > $SOURCE_DIR/$dir/$filename \
+                || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+        else
+            sed "${ln}i \\\n<!-- more -->" "$1" | sed '1,20{/^[#][^#]/d}' | > $SOURCE_DIR/$dir/$filename \
+                || handle_error "Could not write file $SOURCE_DIR/$dir/$filename" $LINENO
+        fi
     fi
 }
 
 function traverse_folder {
     for file in "$1"/*; do
         if [ -d "$file" ]; then
-            echo "traversing folder $file"
+            print_info "traversing folder $file"
             traverse_folder "$file"
         elif [ "${file##*.}" = "md" ]; then
             if [ "$(basename "$file")" == "README.md" ]; then
                 continue
             fi
-            echo "processing file $file"
+            print_info "processing file $file"
             peocess_file "$file"
         fi
     done
@@ -221,7 +273,7 @@ function specify_file {
 # main
 parse_args "$@"
 
-echo "NOTE_DIR: $NOTE_DIR; SOURCE_DIR: $SOURCE_DIR"
+print_info "NOTE_DIR: $NOTE_DIR; SOURCE_DIR: $SOURCE_DIR"
 if [ -n "$FILE" ]; then
     specify_file "$FILE"
 else
